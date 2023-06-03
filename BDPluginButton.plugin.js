185e703ec351a982dd78228fced2ab7142b0e836
notjedhgit config --global user.name notjedhgit config --global user.name notjedh <134240250+notjedh@users.noreply.github.com>
2023-06-03 04:26:33 -0400

    heyy

--- BDPluginButton.plugin.js
+++ BDPluginButton.plugin.js
@@ -13,49 +13,33 @@
 // @source       https://raw.githubusercontent.com/notjedh/myBDRepo/main/BDPluginButton.plugin.js
 // ==/UserScript==
 
+// ==UserScript==
+// @name         BDPluginButton
+// @description  Adds a simple button that leads straight to the BetterDiscord plugins settings page
+// @version      1.0.0
+// @author       NotJedH
+// @source       https://raw.githubusercontent.com/notjedh/myBDRepo/main/BDPluginButton.plugin.js
+// ==/UserScript==
+
 (() => {
+    const buttonId = 'bd-plugin-button';
+
     function createButton() {
-        const buttonContainer = document.createElement('div');
-        buttonContainer.style.position = 'fixed';
-        buttonContainer.style.bottom = '20px';
-        buttonContainer.style.right = '20px';
-        buttonContainer.style.zIndex = '9999';
+        const settingsButton = document.querySelector('[aria-label="User Settings"]');
+        if (!settingsButton) return;
 
-        const button = document.createElement('button');
-        button.innerText = 'BD';
-        button.style.fontFamily = 'discord';
-        button.style.fontSize = '14px';
-        button.style.width = '25px';
-        button.style.height = '25px';
-        button.style.borderRadius = '50%';
-        button.style.backgroundColor = '#7289da';
-        button.style.color = '#fff';
-        button.style.border = 'none';
-        button.style.display = 'flex';
-        button.style.alignItems = 'center';
-        button.style.justifyContent = 'center';
-
-        button.addEventListener('click', () => {
-            const settingsButton = document.querySelector('[aria-label="User Settings"]');
-            if (settingsButton) {
-                settingsButton.click();
-                const observer = new MutationObserver(() => {
-                    const betterDiscordButton = document.querySelector('[aria-label="BetterDiscord"]');
-                    if (betterDiscordButton) {
-                        betterDiscordButton.click();
-                        observer.disconnect();
-                    }
-                });
-                observer.observe(document.body, { childList: true, subtree: true });
-            }
+        const pluginsButton = document.createElement('button');
+        pluginsButton.id = buttonId;
+        pluginsButton.innerText = 'BD';
+        pluginsButton.classList.add('bd-button');
+        pluginsButton.addEventListener('click', () => {
+            BdApi.showPlugins();
         });
 
-        buttonContainer.appendChild(button);
+        const settingsGroup = settingsButton.parentElement;
+        if (!settingsGroup) return;
 
-        const appMount = document.querySelector('#app-mount');
-        if (appMount) {
-            appMount.appendChild(buttonContainer);
-        }
+        settingsGroup.appendChild(pluginsButton);
     }
 
     function waitForDiscord() {
@@ -69,4 +53,3 @@
 
     waitForDiscord();
 })();
-
